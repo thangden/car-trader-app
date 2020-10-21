cf8f38ff2b33244d692a0ea8ac3fbe6f22a322dd
thangden <nmthang.192@gmail.com>
2020-10-21 10:46:17 +0700

    Init project with Create Next App && Typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Car Trader</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Hello from Home</h1>
+      </main>
+    </div>
+  )
+}
